059540bffc966e0e379bd1570f10634f342ed541
CurranWilmot <105678530+CurranWilmot@users.noreply.github.com>
2022-06-07 20:12:10 -0400

    Add files via upload
    Chained methods instead of one by one

--- user.py
+++ user.py
@@ -11,16 +11,19 @@ class User:
 
     def make_deposit(self, amount):
         self.user_balance += amount
+        return self
     
     # Accepts an amount and subtracts that amount to the user's balance attribute
 
     def make_withdrawal(self, amount):
         self.user_balance -= amount
+        return self
 
     # Displays user's name and then prints balance of that user
 
     def display_user_balance(self):
         print(f"User: {self.user_name}, Balance: {self.user_balance}")
+        return self
 
     # Uses make_withdrawal method to subtract an 'amount' from first user
     # Uses make_deposit method to add an 'amount' from second user
@@ -28,6 +31,7 @@ class User:
     def transfer_money(self, other_user, amount):
         self.make_withdrawal(amount)
         other_user.make_deposit(amount)
+        return self
 
 # Creates 3 instances of User object
         
@@ -43,29 +47,14 @@ shirley.display_user_balance()
 
 # Series of transactions for all 3 Users that prints result
 
-monty.make_deposit(100)
-monty.make_deposit(100)
-monty.make_deposit(100)
-monty.make_withdrawal(200)
-monty.display_user_balance()
+monty.make_deposit(100).make_deposit(100).make_deposit(100).make_withdrawal(200).display_user_balance()
 
-bob.make_deposit(300)
-bob.make_deposit(300)
-bob.make_withdrawal(100)
-bob.make_withdrawal(150)
-bob.display_user_balance()
+bob.make_deposit(300).make_deposit(300).make_withdrawal(100).make_withdrawal(150).display_user_balance()
 
-shirley.make_deposit(100000)
-shirley.make_withdrawal(50000)
-shirley.make_withdrawal(10000)
-shirley.make_withdrawal(30000)
-shirley.display_user_balance()
+shirley.make_deposit(100000).make_withdrawal(50000).make_withdrawal(10000).make_withdrawal(30000).display_user_balance()
 
 # Transfers 4000 from one user to another
+# Displays new balances after
 
-monty.transfer_money(shirley, 4000)
-
-# Displays new balances
-
-monty.display_user_balance()
+monty.transfer_money(shirley, 4000).display_user_balance()
 shirley.display_user_balance()
